488051baeca3e44b87f81e030f49da564ce3b3a4
MyNameIsMaxwell <makslyubchuk@mail.ru>
2022-11-08 16:54:38 +0300

    Изменил вид в соответствии с советом IDE

--- config_data/config.py
+++ config_data/config.py
@@ -7,9 +7,12 @@ else:
     load_dotenv('.env.template')
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-RAPID_API_KEY = os.getenv('RAPID_API_KEY')
+headers = {
+    "RAPID_API_KEY": os.getenv('RAPID_API_KEY'),
+    "RAPID_API_HOST": "hotels4.p.rapidapi.com"
+}
 DEFAULT_COMMANDS = (
-    ('lowerprice', "Узнать топ самых дешёвых отелей в городе"),
+    ('lowprice', "Узнать топ самых дешёвых отелей в городе"),
     ('highprice', "Узнать топ самых дорогих отелей в городе"),
     ('bestdeal', "Узнать топ отелей, наиболее подходящих по цене и расположению от центра"),
     ('history', "Узнать историю поиска отелей "),
