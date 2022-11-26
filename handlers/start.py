c37baea125380abce3effabf821b66b12bc28e52
MyNameIsMaxwell <makslyubchuk@mail.ru>
2022-11-26 17:46:29 +0300

    Изменил названия кнопок.

--- handlers/start.py
+++ handlers/start.py
@@ -6,11 +6,11 @@ from loader import bot
 @bot.message_handler(commands=['start'])
 def bot_start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3, one_time_keyboard=True)
-    item1 = types.KeyboardButton("Lowprice")
-    item2 = types.KeyboardButton('Highprice')
-    item3 = types.KeyboardButton("Bestdeal")
-    item4 = types.KeyboardButton("History")
+    item1 = types.KeyboardButton("Lowprice💸")
+    item2 = types.KeyboardButton('Highprice🏢')
+    item3 = types.KeyboardButton("Bestdeal☑️")
+    item4 = types.KeyboardButton("History🕰")
     markup.row(item1, item2, item3)
     markup.row(item4)
-    bot.send_message(message.chat.id, 'Выберите пункт:', reply_markup=markup)
+    bot.send_message(message.chat.id, 'Выберите вариант:', reply_markup=markup)
 
