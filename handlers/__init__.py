ee55387c5df71b51bc2d40cb00c23a2678a6f440
MyNameIsMaxwell <makslyubchuk@mail.ru>
2022-11-26 17:28:05 +0300

    Добавил модуль history_button, который описывает работы кнопки "History"

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,8 +4,11 @@ from . import hello_world
 from . import lowprice
 from . import highprice
 from . import bestdeal
+from . import history_button
 from database import history
 from . import help
 from . import text
+
+
 
 
