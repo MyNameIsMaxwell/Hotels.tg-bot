5eab2dcd92298e84554920a17527973f7bbddc92
MyNameIsMaxwell <makslyubchuk@mail.ru>
2022-11-26 17:42:30 +0300

    Добавил переменную api_headers

--- loader.py
+++ loader.py
@@ -2,6 +2,7 @@ from telebot import TeleBot
 from telebot.storage import StateMemoryStorage
 from config_data import config
 
+
 storage = StateMemoryStorage()
 bot = TeleBot(token=config.BOT_TOKEN, state_storage=storage)
-
+api_headers = {"X-RapidAPI-Key": config.RAPID_API_KEY, "X-RapidAPI-Host": "hotels4.p.rapidapi.com"}
