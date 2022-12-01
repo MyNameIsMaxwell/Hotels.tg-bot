67827b2a600f1a3ae95250079f3028f4e54f7dbb
MyNameIsMaxwell <makslyubchuk@mail.ru>
2022-12-01 20:14:06 +0300

    Добавил документацию.

--- handlers/hello_world.py
+++ handlers/hello_world.py
@@ -3,4 +3,9 @@ from loader import bot
 
 @bot.message_handler(commands=['hello-world'])
 def hello_message(message):
+	"""
+	Функция, реагирующая на команду 'hello-world'.
+
+	:param message: сообщение Telegram
+	"""
 	bot.reply_to(message, f"Привет, <i>{message.from_user.first_name}</i> ✌", parse_mode='HTML')
