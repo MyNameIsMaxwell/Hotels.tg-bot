4593882551762fa9b0dfdb549d4cb39c7cf08ce8
MyNameIsMaxwell <makslyubchuk@mail.ru>
2022-12-01 20:16:46 +0300

    Подключил логгер и условия сохранения в файл логов.

--- loader.py
+++ loader.py
@@ -1,4 +1,5 @@
 from telebot import TeleBot
+from loguru import logger
 from telebot.storage import StateMemoryStorage
 from config_data import config
 
@@ -6,3 +7,4 @@ from config_data import config
 storage = StateMemoryStorage()
 bot = TeleBot(token=config.BOT_TOKEN, state_storage=storage)
 api_headers = {"X-RapidAPI-Key": config.RAPID_API_KEY, "X-RapidAPI-Host": "hotels4.p.rapidapi.com"}
+logger.add(config.LOG_PATH, format="{time} - {level} - {message}", level="DEBUG", rotation="10 MB", compression="zip")
