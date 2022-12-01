25e4f4542ce8e1a00d632d3c3914e5ac285ff328
MyNameIsMaxwell <makslyubchuk@mail.ru>
2022-12-01 20:14:59 +0300

    Добавил документацию и логи. Закинул состояния в отдельный файл.

--- handlers/highprice.py
+++ handlers/highprice.py
@@ -1,7 +1,5 @@
 from telebot import types, custom_filters
-from telebot.types import CallbackQuery
-from telebot.handler_backends import State, StatesGroup
-
+from telebot.types import Message, CallbackQuery
 from datetime import date, timedelta
 from telegram_bot_calendar import DetailedTelegramCalendar
 
@@ -9,36 +7,53 @@ from loader import bot
 from handlers import start
 from database import history
 from utils.searching_hotels import *
-
-
-class MyStates(StatesGroup):
-	city_highprice = State()
-	hotels_count_highprice = State()
-	load_photo_highprice = State()
-	photo_count_highprice = State()
+from states.states import MyStatesHighprice
 
 
 @bot.message_handler(commands=['highprice'])
-def highprice_menu(message):
+@logger.catch()
+def highprice_menu(message: Message) -> None:
+	"""
+	Функция, реагирующая на команду 'highprice' и предлагающая ввести название города.
+
+	:param message: сообщение Telegram
+	"""
 	bot.send_message(message.chat.id, "Давай узнаем топ самых дорогих отелей в городе!\nКакой город?")
-	bot.set_state(message.from_user.id, MyStates.city_highprice, message.chat.id)
+	bot.set_state(message.from_user.id, MyStatesHighprice.city_highprice, message.chat.id)
 
 
-@bot.message_handler(state=MyStates.city_highprice, is_digit=False)
-def city_get(message):
+@bot.message_handler(state=MyStatesHighprice.city_highprice, is_digit=False)
+@logger.catch()
+def city_get(message: Message) -> None:
+	"""
+	Функция, ожидающая корректный ввод города.
+	Состояние пользователя - 'city_highprice', если в названии города не было цифр.
+	Временно записывает данные о городах и об id пользователя для дальнейшей обработки.
+	Показывает клавиатуру с выбором конкретного города для уточнения.
+
+	:param message: сообщение Telegram
+	"""
 	cities = get_city_name_and_id(message.text)
 	with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
 		data['cities'] = cities
 		data['user_id'] = message.from_user.id
 	if cities:
 		bot.send_message(message.from_user.id, 'Пожалуйста, уточните:', reply_markup=print_cities(cities))
-		bot.set_state(message.from_user.id, MyStates.hotels_count_highprice, message.chat.id)
+		bot.set_state(message.from_user.id, MyStatesHighprice.hotels_count_highprice, message.chat.id)
 	else:
 		bot.send_message(message.from_user.id, 'Не нахожу такой город. Введите ещё раз.')
 
 
 @bot.callback_query_handler(lambda call: call.data.startswith('city_id:'))
-def choose_city(call):
+@logger.catch()
+def choose_city(call: CallbackQuery) -> None:
+	"""
+	Функция, реагирующая на нажатие кнопки с выбором конкретного города.
+	Временно записывает данные об id и названии выбранного города для дальнейшей обработки.
+	Предлагает выбрать определенное количество отелей для поиска, либо вернуться в меню.
+
+	:param call: отклик клавиатуры.
+	"""
 	bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
 	bot.delete_message(call.message.chat.id, call.message.message_id)
 
@@ -54,13 +69,28 @@ def choose_city(call):
 
 
 @bot.callback_query_handler(lambda call: call.data == 'exit')
-def back_to_menu(call):
+@logger.catch()
+def back_to_menu(call: CallbackQuery) -> None:
+	"""
+	Функция, реагирующая на нажатие кнопки "Выйти в меню" при выборе варианта с городами.
+	Удаляет состояние и вызывает главное меню.
+
+	:param call: отклик клавиатуры.
+	"""
 	bot.delete_state(call.message.chat.id, call.message.chat.id)
 	start.bot_start(call.message)
 
 
-@bot.message_handler(state=MyStates.hotels_count_highprice, is_digit=False)
-def hotels_count_incorrect(message):
+@bot.message_handler(state=MyStatesHighprice.hotels_count_highprice, is_digit=False)
+@logger.catch()
+def hotels_count_incorrect(message: Message) -> None:
+	"""
+	Функция, воспроизводящаяся при вводе количества отелей не цифрами.
+	Если сообщение имеет значение "Вернуться в меню", то удаляет состояние и вызывает главное меню.
+	Иначе - просит ввести еще раз.
+
+	:param message: сообщение Telegram
+	"""
 	if message.text == "Вернуться в меню":
 		start.bot_start(message)
 		bot.delete_state(message.from_user.id, message.chat.id)
@@ -68,8 +98,16 @@ def hotels_count_incorrect(message):
 		bot.send_message(message.chat.id, 'Не похоже на число. Введите число, пожалуйста')
 
 
-@bot.message_handler(state=MyStates.hotels_count_highprice, is_digit=True)
-def hotels_count_step(message):
+@bot.message_handler(state=MyStatesHighprice.hotels_count_highprice, is_digit=True)
+@logger.catch()
+def hotels_count_step(message: Message) -> None:
+	"""
+	Функция, ожидающая корректный ввод количества отелей в виде цифр и округляющая результат до определенного параметра.
+	Показывает клавиатуру с вопросом о необходимости загрузить фото отелей. Варианты ответа: 'Да' и 'Нет'.
+	Временно записывает данные о количестве отелей для дальнейшей обработки.
+
+	:param message: сообщение Telegram
+	"""
 	hotels_count = message.text
 	if 3 > int(hotels_count):
 		hotels_count = 3
@@ -83,13 +121,22 @@ def hotels_count_step(message):
 	markup.add(item1, item2)
 	markup.add(item3)
 	bot.send_message(message.chat.id, 'Показывать фотографии отеля?', reply_markup=markup)
-	bot.set_state(message.from_user.id, MyStates.load_photo_highprice, message.chat.id)
+	bot.set_state(message.from_user.id, MyStatesHighprice.load_photo_highprice, message.chat.id)
 	with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
 		data['hotels_count'] = hotels_count
 
 
-@bot.message_handler(state=MyStates.load_photo_highprice)
-def load_photo_step(message):
+@bot.message_handler(state=MyStatesHighprice.load_photo_highprice)
+@logger.catch()
+def load_photo_step(message: Message) -> None:
+	"""
+	Функция, реагирующая на нажатие кнопки 'Да' и 'Нет'(либо другой вариант) о необходимости загрузить фото отелей.
+	Если ответ 'да': предлагает выбрать количество фото на клавиатуре.
+	Если ответ 'нет': показывает клавиатуру-календарь с выбором даты заезда.
+	Если ответ 'Вернуться в меню': удаляет состояния и выводит главное меню.
+
+	:param message: сообщение Telegram
+	"""
 	load_photo = message.text
 	if load_photo == u'Да':
 		markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
@@ -98,7 +145,7 @@ def load_photo_step(message):
 		markup.add(item1, item2, item3)
 		markup.add(item4)
 		bot.reply_to(message, "Количество фотографий:", reply_markup=markup)
-		bot.set_state(message.from_user.id, MyStates.photo_count_highprice, message.chat.id)
+		bot.set_state(message.from_user.id, MyStatesHighprice.photo_count_highprice, message.chat.id)
 	elif load_photo == "Вернуться в меню":
 		start.bot_start(message)
 		bot.delete_state(message.from_user.id, message.chat.id)
@@ -111,8 +158,16 @@ def load_photo_step(message):
 						 reply_markup=calendar)
 
 
-@bot.message_handler(state=MyStates.photo_count_highprice, is_digit=True)
-def photo_count_step(message):
+@bot.message_handler(state=MyStatesHighprice.photo_count_highprice, is_digit=True)
+@logger.catch()
+def photo_count_step(message: Message) -> None:
+	"""
+	Функция, ожидающая корректный ввод количества фото в виде цифр и округляющая результат до определенного параметра..
+	Показывает клавиатуру-календарь с выбором даты заезда.
+	Временно записывает данные о количестве разыскиваемых отелей для дальнейшей обработки.
+
+	:param message: сообщение Telegram
+	"""
 	photo_count = message.text
 	if int(photo_count) <= 1:
 		message.text = 1
@@ -130,8 +185,16 @@ def photo_count_step(message):
 		data['photo_count'] = message.text
 
 
-@bot.message_handler(state=MyStates.photo_count_highprice, is_digit=False)
-def photo_count_incorrect(message):
+@bot.message_handler(state=MyStatesHighprice.photo_count_highprice, is_digit=False)
+@logger.catch()
+def photo_count_incorrect(message: Message) -> None:
+	"""
+	Функция, ожидающая некорректный ввод воспроизводящаяся при вводе количества фото не цифрами.
+	Если сообщение имеет значение "Вернуться в меню", то удаляет состояние и вызывает главное меню.
+	Иначе - просит ввести еще раз.
+
+	:param message: сообщение Telegram
+	"""
 	if message.text == "Вернуться в меню":
 		start.bot_start(message)
 		bot.delete_state(message.from_user.id, message.chat.id)
@@ -140,8 +203,15 @@ def photo_count_incorrect(message):
 
 
 @bot.callback_query_handler(func=DetailedTelegramCalendar.func(calendar_id="highprice1"))
-def calen(call: CallbackQuery):
+@logger.catch()
+def calen(call: CallbackQuery) -> None:
+	"""
+	Функция, реагирующая на нажатие кнопки на клавиатуре-календаре о дате въезда.
+	Условия календаря: минимальный вариант выбора даты - завтра, максимальный - два года.
+	Временно записывает данные о дате въезда для дальнейшей обработки.
 
+	:param call: отклик клавиатуры.
+	"""
 	result, key, step = DetailedTelegramCalendar(min_date=date.today() + timedelta(days=1),
 												 max_date=date.today() + timedelta(days=730), locale='ru', calendar_id="highprice1").process(
 		call.data)
@@ -167,7 +237,17 @@ def calen(call: CallbackQuery):
 
 
 @bot.callback_query_handler(func=DetailedTelegramCalendar.func(calendar_id="highprice2"))
-def calen(call: CallbackQuery):
+@logger.catch()
+def calen(call: CallbackQuery) -> None:
+	"""
+	Функция, реагирующая на нажатие кнопки на клавиатуре-календаре о дате выезда.
+	Условия календаря: минимальный вариант выбора даты - на следующий день после даты въезда,
+	максимальный - два года от даты въезда.
+	Временно записывает данные о дате выезда для дальнейшей обработки.
+	Вызывает функцию, позволяющую получить результат поиска по временным данным, полученных от пользователя.
+
+	:param call: отклик клавиатуры.
+	"""
 	with bot.retrieve_data(call.message.chat.id, call.message.chat.id) as data:
 		date_from = data['dateIn']
 		result, key, step = DetailedTelegramCalendar(min_date=date_from + timedelta(days=1),
@@ -190,7 +270,15 @@ def calen(call: CallbackQuery):
 		bot.delete_state(call.message.chat.id, call.message.chat.id)
 
 
-def ready_for_answer_highprice(message):
+@logger.catch()
+def ready_for_answer_highprice(message: Message) -> None:
+	"""
+	Функция обрабатывает полученные данные, делает запрос на парсинг отелей, выводит полученный результат
+	в зависимости от того, желал ли пользователь видеть фотографии или нет, и добавляет результат в БД.
+	Если в результате получает ошибку - возвращает в главное меню и просит пользователя ввести данные дальнейшую команду.
+
+	:param message: сообщение Telegram
+	"""
 	info_for_history = list()
 	with bot.retrieve_data(message.chat.id, message.chat.id) as data:
 		msg = (f"<b>Город: {data['city']}\n"
@@ -199,15 +287,15 @@ def ready_for_answer_highprice(message):
 		try:
 			photo_count_exist = f"Фотографии: {data['photo_count']} шт.\n</b>"
 			msg = msg[:-4] + photo_count_exist
-			photo_count = data['photo_count']
+			photo_count: int = data['photo_count']
 		except:
 			pass
-		user_id = data['user_id']
-		hotels_count = data['hotels_count']
-		date_in = data['dateIn']
-		date_out = data['dateOut']
-		city_name = data['city']
-		city_id = data['city_id']
+		user_id: int = data['user_id']
+		hotels_count: str = data['hotels_count']
+		date_in: date = data['dateIn']
+		date_out: date = data['dateOut']
+		city_name: str = data['city']
+		city_id: str = data['city_id']
 		bot.send_message(message.chat.id, msg, parse_mode="html")
 
 	if 'photo_count' not in locals():
