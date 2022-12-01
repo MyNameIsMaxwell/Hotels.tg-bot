b5dc22b3455344a731ecbbb4e664f9c78e532fa3
MyNameIsMaxwell <makslyubchuk@mail.ru>
2022-12-01 20:14:10 +0300

    Добавил документацию.

--- handlers/help.py
+++ handlers/help.py
@@ -4,5 +4,10 @@ from loader import bot
 
 @bot.message_handler(commands=['help'])
 def bot_help(message):
+    """
+    Функция, реагирующая на команду 'help'. Выводит сообщение со списком команд.
+
+    :param message: сообщение Telegram
+    """
     text = [f'/{command} - {desk}' for command, desk in DEFAULT_COMMANDS if command != 'help']
     bot.reply_to(message, '\n'.join(text))
