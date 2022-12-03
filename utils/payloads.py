118f78658848fa396353270a8fe24de47ef7d3a3
MyNameIsMaxwell <makslyubchuk@mail.ru>
2022-12-03 14:10:09 +0300

    Условия для поиска отеля для разных запросов.

---
+++ utils/payloads.py
@@ -0,0 +1,104 @@
+def low_high_price_payload(command, city_id, hotels_count, date_in, date_out):
+	year_in, month_in, day_in = date_in.split("-")
+	if day_in[0] == '0':
+		day_in = day_in[1]
+	year_out, month_out, day_out = date_out.split("-")
+	if day_out[0] == '0':
+		day_out = day_out[1]
+
+	if command == "lowprice":
+		lowprice_payload = {
+			"currency": "USD",
+			"locale": "ru_RU",
+			"destination": {
+				"regionId": city_id
+			},
+			"checkInDate": {
+				"day": int(day_in),
+				"month": int(month_in),
+				"year": int(year_in)
+			},
+			"checkOutDate": {
+				"day": int(day_out),
+				"month": int(month_out),
+				"year": int(year_out)
+			},
+			"rooms": [
+				{
+					"adults": 1
+				}
+			],
+			"resultsStartingIndex": 0,
+			"resultsSize": int(hotels_count),
+			"sort": "PRICE_LOW_TO_HIGH"
+		}
+		return lowprice_payload
+	else:
+		highprice_payload = {
+			"currency": "USD",
+			"locale": "ru_RU",
+			"destination": {
+				"regionId": city_id
+			},
+			"checkInDate": {
+				"day": int(day_in),
+				"month": int(month_in),
+				"year": int(year_in)
+			},
+			"checkOutDate": {
+				"day": int(day_out),
+				"month": int(month_out),
+				"year": int(year_out)
+			},
+			"rooms": [
+				{
+					"adults": 1
+				}
+			],
+			"resultsStartingIndex": 0,
+			"resultsSize": hotels_count,
+			"sort": "PRICE_HIGH_TO_LOW"
+		}
+		return highprice_payload
+
+
+def bestdeal_payload(city_id, hotels_count, price, date_in, date_out):
+	year_in, month_in, day_in = date_in.split("-")
+	if day_in[0] == '0':
+		day_in = day_in[1]
+	year_out, month_out, day_out = date_out.split("-")
+	if day_out[0] == '0':
+		day_out = day_out[1]
+
+	best_payload = {
+		"currency": "USD",
+		"locale": "ru_RU",
+		"destination": {
+			"regionId": city_id
+		},
+		"checkInDate": {
+			"day": int(day_in),
+			"month": int(month_in),
+			"year": int(year_in)
+		},
+		"checkOutDate": {
+			"day": int(day_out),
+			"month": int(month_out),
+			"year": int(year_out)
+		},
+		"rooms": [
+			{
+				"adults": 1
+			}
+		],
+		"resultsStartingIndex": 0,
+		"resultsSize": 50,
+		"sort": "DISTANCE",
+		"filters": {
+			"price": {
+				"max": price,
+				"min": 1
+			}
+		}
+	}
+	return best_payload
